c81c9c5d4a7682a73dd7368f6770e0daaa59ec0b
Alice J Zheng <alice.j.zheng@cn.pwc.com>
2022-08-16 17:37:51 +0800

    登录页面添加注册/忘记密码/登录界面

--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,20 +1,33 @@
 <template>
   <el-row class="login" justify="center" align="middle">
-    <el-col :span="6">
-      <el-card>
-        <el-image style="width: 100px;" :src="logoUrl" />
+    <el-col :span="8">
+      <el-card :header="title">
+        <el-image v-if="type=='login'" style="width: 100px;" :src="logoUrl" />
+        {{test}}
         <div>
-          <el-form ref="loginForm" :model="loginForm" status-icon label-width="40px">
-            <el-form-item label="账户" prop="name">
-              <el-input v-model="loginForm.name" />
+          <el-form ref="loginForm" :model="loginForm" status-icon label-width="80px" :rules="rules">
+            <el-form-item label="账户" prop="name" v-if="type !='forget'">
+              <el-input v-model="loginForm.name" :placeholder="loginFormPlaceholder.namePlaceholder" />
             </el-form-item>
-            <el-form-item label="密码" prop="password">
-              <el-input v-model="loginForm.password" />
+            <el-form-item label="邮箱" prop="email" v-if="type !='login'">
+              <el-input v-model="loginForm.email" type="email" :placeholder="loginFormPlaceholder.emailPlaceholder" />
             </el-form-item>
-            <el-form-item>
-              <el-button>忘记密码</el-button>
-              <el-button>注册</el-button>
-              <el-button type="primary">登录</el-button>
+            <el-form-item :label="passwordLabel" prop="password">
+              <el-input v-model="loginForm.password" type="password"
+                :placeholder="loginFormPlaceholder.passPlaceholder" />
+            </el-form-item>
+            <el-form-item label="确认密码" prop="confirmPassword" v-if="type!='login'">
+              <el-input v-model="loginForm.confirmPassword" type="password"
+                :placeholder="loginFormPlaceholder.passPlaceholder" />
+            </el-form-item>
+            <el-form-item v-if="type=='login'">
+              <el-button @click="clear('forget')">忘记密码</el-button>
+              <el-button @click="clear('register')">注册</el-button>
+              <el-button type="primary" @click="clear('login')">登录</el-button>
+            </el-form-item>
+            <el-form-item v-if="type!='login'">
+              <el-button @click="clear('login')">取消</el-button>
+              <el-button type="primary" @click="sure">确认</el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -24,23 +37,124 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      logoUrl: "/logo.webp",
-      loginForm: {
-        name: "",
-        password: ""
+  const require = "必填"
+  export default {
+    data() {
+      const validateName = (rule, value, callback) => {
+        if (!/^[\u4e00-\u9fa5a-zA-Z]{3,10}$/.test(value)) {
+          callback(new Error(this.loginFormPlaceholder.namePlaceholder))
+        }
+        callback()
+      }
+
+      const validatePass = (rule, value, callback) => {
+        if (!/^[0-9a-zA-Z]{6,12}$/.test(value)) {
+          callback(new Error(this.loginFormPlaceholder.passPlaceholder))
+        }
+        callback()
+      }
+
+      const validateConfirmpass = (rule, value, callback) => {
+        if (!/^[0-9a-zA-Z]{6,12}$/.test(value)) {
+          callback(new Error(this.loginFormPlaceholder.passPlaceholder))
+        }
+
+        if(value != this.loginForm.password) callback(new Error(this.loginFormPlaceholder.confirmpassPlaceholder))
+        callback()
+      }
+
+      return {
+        logoUrl: "/logo.webp",
+        type: "login", //取值：register,forget
+        loginForm: {
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: ""
+        },
+        initLoginForm:{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: ""
+        },
+        loginFormPlaceholder: {
+          namePlaceholder: "3-10个英文或中文字符",
+          emailPlaceholder: "请输入正确邮箱地址",
+          passPlaceholder: "6-12个英文和数字组成的字符",
+          confirmpassPlaceholder:"密码错误，请重新输入"
+        },
+        rules: {
+          name: [{
+              required: true,
+              message: require,
+              trigger: 'blur'
+            },
+            {
+              validator: validateName,
+              trigger: 'change'
+            },
+          ],
+          email: [{
+              required: true,
+              message: require,
+              trigger: 'blur'
+            },
+            {
+              type: 'email',
+              message:'请输入正确邮箱地址',
+              trigger: ['blur','change']
+            },
+          ],
+          password: [{
+              required: true,
+              message: require,
+              trigger: 'blur'
+            },
+            {
+              validator: validatePass,
+              trigger: 'change'
+            },
+          ],
+          confirmPassword: [{
+              required: true,
+              message: require,
+              trigger: 'blur'
+            },
+            {
+              validator: validateConfirmpass,
+              trigger: ['blur','change']
+            },
+          ],
+        }
+      }
+    },
+    computed: {
+      title() {
+        return this.type == "register" ? "用户注册" : ((this.type == "forget") ? "忘记密码" : null)
+      },
+      passwordLabel() {
+        return this.type == 'forget' ? '新密码' : '密码'
+      }
+    },
+    methods: {
+      sure() {
+        //this.type = 'login'
+        console.log('111 loginForm = ',this.loginForm)
+      },
+      clear(type){
+        this.type = type
+        this.loginForm = this.initLoginForm
+        this.$refs.loginForm.resetFields()
       }
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-.login {
-  width: calc(100%);
-  height: calc(100%);
-  background: url('/banner.png') no-repeat center;
-}
+  .login {
+    width: calc(100%);
+    height: calc(100%);
+    background: url('/banner.png') no-repeat center;
+  }
 </style>
